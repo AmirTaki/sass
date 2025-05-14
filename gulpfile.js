d02758338ec9ae68c8b7253f077daa6c77784d22
amir <amirtaki623@gmail.com>
2025-05-14 20:34:09 +0330

    import at sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const {src, dest, watch , series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 
-function buildStyles () {
-    return src('style.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+function buildStyles() {
+    return src('*.scss')
+        .pipe(sass())
+        .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch (['style.scss'], buildStyles)
+    watch (['*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
